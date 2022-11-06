c5ff31510308be574ad19eab15ca85da52d38041
BRAULIO ALVAREZ <braulio-alvarez@hotmail.com>
2022-11-06 18:37:05 -0400

    Clearing sapce

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Cual es tu nombre");
-
-
 string nombre = Console.ReadLine();
 Console.WriteLine($"Hola Bienverido :{nombre}");
-
 
 Console.WriteLine(DateTime.Now);
 
